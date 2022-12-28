d1ef5041cbf76d98dc081421f51101fbbfbad890
Victor Lourenço <victor.lourenco@tgid.com.br>
2022-12-28 09:53:59 -0300

    LEARN: Processando dados atraves de variaveis e realizando o casting

---
+++ B.Estrutura.Sequencial/src/b/processamento/dados/casting/Main.java
@@ -0,0 +1,37 @@
+package b.processamento.dados.casting;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x;
+		double y;
+		
+		x = 5;
+		y = 2.5 * x;
+		
+		System.out.println(y);
+		
+		int a, b;
+		double resultado;
+		
+		a = 5; 
+		b = 2;
+		
+		resultado = (double) a/b;
+		
+		System.out.println(resultado);
+		
+		double d;
+		int i;
+		
+		d = 5.0;
+		i = (int) d;
+		
+		System.out.println(i);
+		
+		
+		
+	}
+	
+}
